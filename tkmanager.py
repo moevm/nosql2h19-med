4f5449742568cf4c8d4b589f489bb0a871b2f349
Андрей Вайгачёв <qwembe@gmail.com>
2019-12-11 00:12:45 +0300

    adding symptoms to the list in rec frame

--- tkmanager.py
+++ tkmanager.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 import frames
 from pandastable import Table, TableModel
+from pandas import DataFrame
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg  # , NavigationToolbar2TkAgg
+import numpy as np
 
 
 class tkManager:
@@ -24,13 +26,29 @@ class tkManager:
 
         self.t1 = frames.TableFrame(self.container)
         self.p1 = frames.PlotFrame(self.container)
-        self.frrec = frames.RecFrame(self.container)
+        self.frrec = frames.RecFrame(self.container, self.do_search)
         self.ief = frames.ImportExportFrame(self.container, self.do_imp, self.do_exp)
         self.stat = frames.StatFrame(self.container, click=lambda opt: self.clik(opt))
         self.comm = frames.CommonStatFrame(self.container)
 
     def setbd(self, gman):
         self.gman = gman
+
+    def do_search(self, id, tables):
+        print("Searching for diagnoses!")
+        sym, diag = tables
+        df = sym.get_model().df
+        if not df.empty:
+            ids = df[df.columns[1]].to_numpy()
+            id = np.append(id, ids)
+            print(id)
+            str = np.array2string(id, separator=" , ")
+            data = self.gman.get_sym_via_ids(str)
+            sym.updateData(data)
+        else:
+            data = self.gman.get_sym_via_ids([id])
+            sym.updateData(data)
+            
 
     def clik(self, opt):
         if self.container is None:
@@ -75,9 +93,9 @@ class tkManager:
         # print(resp)
         t2.updateData(resp)
         resp = self.gman.loadCSV_dyf()
-        # print(resp)
+        print(resp)
         t3.updateData(resp)
-        print("im importing!")
+        # print("im importing!")
         return 0
 
     def do_exp(self, tables):
